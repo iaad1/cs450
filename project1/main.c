c6aaedacbcbe02c867cffc89ddbe092155a43930
j.tanninen3 <j.tanninen3@students.clark.edu>
2023-09-07 16:08:31 -0700

    got reading of stdin into array, researching how to do diagonalization

--- project1/main.c
+++ project1/main.c
@@ -7,6 +7,19 @@
 #include <stdio.h>
 
 int main() {
+    int n; // Matrix size.
+    // Get the size of the arr.
+    scanf("%d", &n);
+    double arr[n][n];
+
+    // Double for loop for getting matrix
+    for (int i=0; i < n; i++) {
+        for (int j=0; i < n; i++) {
+            scanf("%lf", &arr[i][j]);
+        }
+    }
+
     
+
     return 0;
 }
