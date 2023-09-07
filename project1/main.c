942d68a0f29c1c5fcc0fca01e7b1f171c126a193
j.tanninen3 <j.tanninen3@students.clark.edu>
2023-09-07 13:02:21 -0700

    created boiler plate for p1

---
+++ project1/main.c
@@ -0,0 +1,11 @@
+// Project 1
+// Judah Tanninen
+// Description: Finds the determinant of a given matrix
+// Must be of size n x n
+
+// Includes
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+}
