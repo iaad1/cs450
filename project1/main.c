a9a9489af1cd03129a882174cc99b08af8bde625
j.tanninen3 <j.tanninen3@students.clark.edu>
2023-09-12 15:07:09 -0700

    got it all working, see here for version with printArr

--- project1/main.c
+++ project1/main.c
@@ -30,24 +30,40 @@ int main() {
             scanf("%lf", &arr[i][j]);
         }
     }
-    printf("test\n");
-    int swaps;
+    double sum = 1;
     double *temp; // Temp pointer for row swaps.
 
     // Loop through the columns, beginning at the first
     for (int i=0; i < n; i++) {
         // Normalize the pivot position
         for (int j=i; j < n; j++) { // Start at the pivot
-            double el = arr[i][j];
+            double el = arr[j][i];
             if (el == 0) continue; // Skip if zero, need a value
-            // Swap this column so theres a value at the pivot.
+            if (i == j) continue;
+            // // Swap this column so theres a value at the pivot.
             temp = arr[j];
             arr[j] = arr[i];
             arr[i] = temp;
+            sum *= -1;
+            break;
         }
     }
-    printArr(arr, n);
+    // Should be normalized now
 
+    // Using forward elimination, make all elements below the pivot elements zero.
+    for (int i=0; i < n; i++) {
+        double pivot = arr[i][i];
+        sum *= pivot;
+        for (int j=i+1; j < n; j++) {
+            double *row = arr[j];
+            double el = row[i];
+            double mult = el / pivot;
+            for (int k=i; k < n; k++) {
+                arr[j][k] -= mult * arr[i][k];
+            }
+        }
+    }
 
+    printf("%lf\n", sum);
     return 0;
 }
