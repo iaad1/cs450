d258db2e2aa0fdf8627bc9c95136bf67520368ba
j.tanninen3 <j.tanninen3@students.clark.edu>
2023-09-12 15:18:47 -0700

    added a bunch of comments

--- project1/main.c
+++ project1/main.c
@@ -7,16 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void printArr(double **arr, int n) {
-    for (int i=0; i < n; i++) {
-        for (int j=0; j < n; j++)
-            printf("%lf ", arr[i][j]);
-        printf("\n");
-    }
-    
-}
-
 int main() {
     int n; // Matrix size.
     // Get the size of the arr.
@@ -27,10 +17,10 @@ int main() {
     for (int i=0; i < n; i++) {
         arr[i] = malloc(n * sizeof(double));
         for (int j=0; j < n; j++) {
-            scanf("%lf", &arr[i][j]);
+            scanf("%lf", &arr[i][j]); // Scan the double into the array.
         }
     }
-    double sum = 1;
+    double determinant = 1; // Start the determinant at positive one.
     double *temp; // Temp pointer for row swaps.
 
     // Loop through the columns, beginning at the first
@@ -39,12 +29,12 @@ int main() {
         for (int j=i; j < n; j++) { // Start at the pivot
             double el = arr[j][i];
             if (el == 0) continue; // Skip if zero, need a value
-            if (i == j) continue;
+            if (i == j) continue; // Skip if pivot row and has value, useless swap.
             // // Swap this column so theres a value at the pivot.
             temp = arr[j];
             arr[j] = arr[i];
             arr[i] = temp;
-            sum *= -1;
+            determinant *= -1; // Row swapping, determinant sign flips.
             break;
         }
     }
@@ -52,18 +42,18 @@ int main() {
 
     // Using forward elimination, make all elements below the pivot elements zero.
     for (int i=0; i < n; i++) {
-        double pivot = arr[i][i];
-        sum *= pivot;
-        for (int j=i+1; j < n; j++) {
+        double pivot = arr[i][i]; // Pivot element
+        determinant *= pivot; // Becuase we are eliminating forward, we can just grab the value here.
+        for (int j=i+1; j < n; j++) { // Below is forward elimination.
             double *row = arr[j];
             double el = row[i];
             double mult = el / pivot;
             for (int k=i; k < n; k++) {
-                arr[j][k] -= mult * arr[i][k];
+                arr[j][k] -= mult * arr[i][k]; // Subtract the row by a scalar of another row. (may actually be columns)
             }
         }
     }
 
-    printf("%lf\n", sum);
+    printf("%lf\n", determinant); // Print the output.
     return 0;
 }
